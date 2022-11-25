4cf9bf312a6bcbde76cececc2d4ccce3b3b14f1a
Timothée Fleury <timothee@fromsmash.com>
2022-11-25 14:42:01 +0100

    Remove useless file, Add .gitignore

--- dist/globals/constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DownloaderEvent = void 0;
-var DownloaderEvent;
-(function (DownloaderEvent) {
-    DownloaderEvent["Progress"] = "progress";
-})(DownloaderEvent = exports.DownloaderEvent || (exports.DownloaderEvent = {}));
-//# sourceMappingURL=constant.js.map
--- dist/helpers/isFunction.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function isFunction(functionToCheck: any): boolean;
